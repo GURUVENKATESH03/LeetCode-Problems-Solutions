d962d17a3715d308815acec783f37888d44d4946
GURUVENKATESH03 <146409090+GURUVENKATESH03@users.noreply.github.com>
2024-05-20 19:01:40 +0530

    Update 0941-sort-array-by-parity.java

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.java
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.java
@@ -5,17 +5,14 @@ class Solution {
         int right = size - 1;
 
         while (left < right) {
-            // Find the first odd number from the left
             while (left < right && nums[left] % 2 == 0) {
                 left++;
             }
             
-            // Find the first even number from the right
             while (left < right && nums[right] % 2 != 0) {
                 right--;
             }
 
-            // Swap if found
             if (left < right) {
                 int temp = nums[left];
                 nums[left] = nums[right];
